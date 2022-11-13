e2c260112a2ffa292b7f3716ce533337e75532d8
zaqsaw <zaqsaw@gmail.com>
2022-11-13 00:27:58 +0000

    add an argument for the file name

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # Downloads m3u8 stream to /tmp/stream.mp4
 
 m3u8=$1
-DOCKER_PROCESS=$(docker run --volume /tmp:/app/download --env M3U8=${m3u8} --env NAME='/app/download/stream' --env EXT='mp4' -d streamdl:latest)
+name=$2
+DOCKER_PROCESS=$(docker run --volume /tmp:/app/download --env M3U8=${m3u8} --env NAME=/app/download/${name} --env EXT='mp4' -d streamdl:latest)
 docker logs -f ${DOCKER_PROCESS}
 
